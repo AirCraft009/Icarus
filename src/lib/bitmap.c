e964b291060bbe09eb5ab0b6c945975ff67adf87
AirCraft009 <coconraupe@gmail.com>
2026-09-22 20:43:17 +0200

    bitmap extension

--- src/lib/bitmap.c
+++ src/lib/bitmap.c
@@ -47,3 +47,43 @@ byte bitmap_get(bitmap *map, uint64_t index) {
 
     return (map->data[byte_ind] & (1 << offset)) >> offset;
 }
+
+int bitmap_set_values(bitmap *map, uint64_t startIndex, uint64_t len) {
+    if ((startIndex + len) > map->size) {
+        return -1;
+    }
+    uint64_t i;
+    for (i = startIndex; i < startIndex + len - 8; i += 8) {
+        uint64_t byte_ind = i / 8;
+
+        map->data[byte_ind] = 0xFF;
+    }
+
+    for (i = startIndex + len - 8; i < startIndex + len; i ++) {
+        uint64_t byte_ind = i / 8;
+        uint64_t offset = i % 8;
+
+        map->data[byte_ind] |= (1 << offset);
+    }
+    return 0;
+}
+
+int bitmap_clear_values(bitmap *map, uint64_t startIndex, uint64_t len) {
+    if ((startIndex + len) > map->size) {
+        return -1;
+    }
+    uint64_t i;
+    for (i = startIndex; i < startIndex + len - 8; i += 8) {
+        uint64_t byte_ind = i / 8;
+
+        map->data[byte_ind] = 0;
+    }
+
+    for (i = startIndex + len - 8; i < startIndex + len; i ++) {
+        uint64_t byte_ind = i / 8;
+        uint64_t offset = i % 8;
+
+        map->data[byte_ind] &= ~(1 << offset);
+    }
+    return 0;
+}
